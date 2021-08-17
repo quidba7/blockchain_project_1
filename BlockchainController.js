f5b2afb7a2fb4104e4133ab05bf6db2b2f339843
quidba7 <baptistequidet@gmail.com>
2021-08-17 07:02:33 +0000

    add Blockchain controller

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -115,6 +116,19 @@ class BlockchainController {
                 return res.status(500).send("Block Not Found! Review the Parameters!");
             }
             
+        });
+    }
+
+    // This endpoint allows you to request the validation of list of stars
+    validateChain() {
+        this.app.get("/validateChain", async (req, res) => {
+            let errors = await this.blockchain.validateChain();
+            if (errors.length > 0) {
+                return res.status(500).json(errors);
+            }
+            else {
+                return res.status(200).send(this.blockchain.height + ' blocks successfully validated.');
+            }
         });
     }
 
